983d52a02168d9e189d51a98c881f12059558e7d
javiergespejo <javiergespejo@gmail.com>
2020-09-12 20:44:55 -0300

    DTO added

---
+++ MusicLibrary/DTO/ArtistDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.DTO
+{
+    public class ArtistDTO
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
